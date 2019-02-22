a716db76464702f913c26a56d054aaedaea8244a
chanyowthea <454982165@qq.com>
2019-02-22 10:54:20 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string path = @"D:\FF\Test\HotFixServer2\bin\Debug\Files\";
+            //// D:\FF\Test\HotFixServer2\bin\Debug\Files\HotUpdateDemo_Server\Assets\StreamingAssets\AssetBundles
+            //using (Stream fs = File.Open(path + "HotUpdateDemo_Server/Assets/StreamingAssets/AssetBundles/bot", FileMode.Open))
+            //{
+            //    Console.WriteLine("fs.Length=" + fs.Length); 
+            //}
+            //Console.ReadKey(); 
+
+            Init();
+        }
+
+        private static HttpServer http;
+        /// <summary>
+        /// 服务器初始化
+        /// </summary>
+        public static void Init()
+        {
+            http = new HttpServer(8080);
+            http.Start();
+        }
+
+        /// <summary>
+        /// 服务器关闭
+        /// </summary>
+        public static void Close()
+        {
+            http.Stop(); http = null;
+        }
+    }
+}
